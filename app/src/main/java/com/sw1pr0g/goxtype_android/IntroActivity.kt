3704d4d01e38f60ae1654e8cc865a457ece69844
SW1pr0g <poryadin_alex@mail.ru>
2023-02-02 23:35:39 +0300

    Creating branch add-intro

--- app/src/main/java/com/sw1pr0g/goxtype_android/IntroActivity.kt
+++ app/src/main/java/com/sw1pr0g/goxtype_android/IntroActivity.kt
@@ -3,9 +3,9 @@ package com.sw1pr0g.goxtype_android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_intro)
     }
 }
