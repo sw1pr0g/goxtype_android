6d8d0fc5bfb00f8d74cf16effaa9d386bae554da
SW1pr0g <poryadin_alex@mail.ru>
2023-02-06 09:18:18 +0300

    Add first design to intro-layout

--- app/src/main/java/com/sw1pr0g/goxtype_android/MainActivity.kt
+++ app/src/main/java/com/sw1pr0g/goxtype_android/MainActivity.kt
@@ -3,9 +3,9 @@ package com.sw1pr0g.goxtype_android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class IntroActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        setContentView(R.layout.activity_main)
     }
 }
