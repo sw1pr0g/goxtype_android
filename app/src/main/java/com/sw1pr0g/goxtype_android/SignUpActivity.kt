4ca1227fce5128518bc94cc29f088dc336b479ca
Алексей Порядин <aleksejporadin@Alexanders-MacBook-Pro.local>
2023-02-27 17:05:48 +0300

    Add slider from LogIn to SignUp

--- app/src/main/java/com/sw1pr0g/goxtype_android/SignUpActivity.kt
+++ app/src/main/java/com/sw1pr0g/goxtype_android/SignUpActivity.kt
@@ -11,8 +11,14 @@ class SignUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
-        val onLogInTextClick = findViewById<ImageView>(R.id.logInArrow)
+        val onLogInTextClick = findViewById<TextView>(R.id.logInText)
         onLogInTextClick.setOnClickListener {
+            val intent = Intent(this, LogInActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.slide_in_left, R.anim.stay)
+        }
+        val onLogInArrowClick = findViewById<ImageView>(R.id.logInArrow)
+        onLogInArrowClick.setOnClickListener {
             val intent = Intent(this, LogInActivity::class.java)
             startActivity(intent)
             overridePendingTransition(R.anim.slide_in_left, R.anim.stay)
