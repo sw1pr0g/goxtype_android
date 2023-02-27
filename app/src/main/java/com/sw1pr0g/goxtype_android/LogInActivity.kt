7ca38b807bb17eb60eea627cee1c4e8dc4f6d359
Алексей Порядин <aleksejporadin@Alexanders-MacBook-Pro.local>
2023-02-27 14:09:40 +0300

    Add Listeners to Activity

--- app/src/main/java/com/sw1pr0g/goxtype_android/LogInActivity.kt
+++ app/src/main/java/com/sw1pr0g/goxtype_android/LogInActivity.kt
@@ -1,11 +1,27 @@
 package com.sw1pr0g.goxtype_android
 
+import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class LogInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+
+        val onSignUpTextClick = findViewById<TextView>(R.id.signUpText)
+        onSignUpTextClick.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+        val onSignUpPlusClick = findViewById<ImageView>(R.id.signUpPlus )
+        onSignUpPlusClick.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
