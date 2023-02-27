27ddc59d343c934d914f6e03f75355c44e4fe4df
Алексей Порядин <aleksejporadin@Alexanders-MacBook-Pro.local>
2023-02-27 15:21:13 +0300

    Fix Status Bar

--- app/src/main/java/com/sw1pr0g/goxtype_android/LogInActivity.kt
+++ app/src/main/java/com/sw1pr0g/goxtype_android/LogInActivity.kt
@@ -1,18 +1,22 @@
 package com.sw1pr0g.goxtype_android
 
 import android.content.Intent
-import android.media.Image
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
-import android.widget.Button
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class LogInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
 
+        //ClickListeners
         val onSignUpTextClick = findViewById<TextView>(R.id.signUpText)
         onSignUpTextClick.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
@@ -22,6 +26,7 @@ class LogInActivity : AppCompatActivity() {
         onSignUpPlusClick.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
+            overridePendingTransition(R.anim.slide_in_right, R.anim.stay)
         }
     }
 }
