ef3561f5fa987973e3719c01eb42f2b27208d524
Alex Poryadin <poryadin_alex@mail.ru>
2023-03-02 10:21:20 +0300

    MainUI created, profile visible

--- app/src/main/java/com/sw1pr0g/goxtype_android/MainFragment.kt
+++ app/src/main/java/com/sw1pr0g/goxtype_android/MainFragment.kt
@@ -1,13 +1,28 @@
 package com.sw1pr0g.goxtype_android
 
+import android.content.Intent
+import android.media.Image
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
 
 class MainFragment : Fragment() {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val profileImage: ImageButton = view.findViewById(R.id.profileImage)
+
+        profileImage.setOnClickListener {
+            val intent = Intent(activity, ProfileActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
