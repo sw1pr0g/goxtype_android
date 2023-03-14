e764c92c5d15ee53c0deb65c4af29a1271a69900
Alex Poryadin <poryadin_alex@mail.ru>
2023-03-14 17:28:40 +0300

    Add API to project, error with local connection on device

--- app/src/main/java/com/sw1pr0g/goxtype_android/AuthSignUpFragment.kt
+++ app/src/main/java/com/sw1pr0g/goxtype_android/AuthSignUpFragment.kt
@@ -5,10 +5,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.content.ContextCompat.getColor
 import androidx.fragment.app.Fragment
+import com.sw1pr0g.goxtype_android.api.ApiInterface
+import com.sw1pr0g.goxtype_android.api.RetrofitInstance
+import com.sw1pr0g.goxtype_android.api.UserBody
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class AuthSignUpFragment: Fragment() {
 
@@ -24,6 +34,13 @@ class AuthSignUpFragment: Fragment() {
     private lateinit var logInTextView: TextView
     private lateinit var logInImageView: ImageView
 
+    private lateinit var nameEditText: EditText
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+
+    private lateinit var signUpButton: Button
+
+
     override fun onAttach(context: Context) {
         super.onAttach(context)
         callbacks = context as Callbacks?
@@ -38,9 +55,22 @@ class AuthSignUpFragment: Fragment() {
 
         logInTextView = view.findViewById(R.id.log_in_text_view)
         logInImageView = view.findViewById(R.id.log_in_image_view)
+        signUpButton = view.findViewById(R.id.sign_up_button)
+
+        nameEditText = view.findViewById(R.id.name_edit_text)
+        emailEditText = view.findViewById(R.id.email_edit_text)
+        passwordEditText = view.findViewById(R.id.password_edit_text)
+
 
         logInTextView.setOnClickListener { showLogInFragment() }
         logInImageView.setOnClickListener { showLogInFragment() }
+        signUpButton.setOnClickListener {
+            signUp(nameEditText.text.toString(),
+                   emailEditText.text.toString(),
+                   passwordEditText.text.toString())
+        }
+
+
 
         return view
     }
@@ -53,5 +83,33 @@ class AuthSignUpFragment: Fragment() {
     private fun showLogInFragment() = callbacks?.showFragment(AuthLogInFragment(),
         getColor(requireActivity(), R.color.background), true,
         firstShowing = false)
+
+    private fun signUp(email: String,
+                       password: String,
+                       name: String) {
+
+        val retIn = RetrofitInstance.getRetrofitInstance().create(ApiInterface::class.java)
+        val signUpInfo = UserBody(email, password, name)
+
+        retIn.signUp(signUpInfo).enqueue(object :
+            Callback<ResponseBody> {
+
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                Toast.makeText(activity, t.message, Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+
+                if (response.code() == 201) {
+                    Toast.makeText(activity, "SignUp Success", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(activity, "ERROR! SignUp Non Success", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+            })
+
+    }
 
 }
--- app/src/main/java/com/sw1pr0g/goxtype_android/api/RetrofitInstance.kt
+++ app/src/main/java/com/sw1pr0g/goxtype_android/api/RetrofitInstance.kt
@@ -1,4 +1,45 @@
 package com.sw1pr0g.goxtype_android.api
 
+import okhttp3.OkHttpClient
+import okhttp3.ResponseBody
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
+interface ApiInterface {
+
+    @Headers("Content-Type:application/json")
+    @POST("auth_tokens")
+    fun logIn(@Body info: LogInBody): retrofit2.Call<ResponseBody>
+
+    @Headers("Content-Type:application/json")
+    @POST("users")
+    fun signUp(
+        @Body info: UserBody
+    ): retrofit2.Call<ResponseBody>
+
+}
+
 class RetrofitInstance {
+    companion object {
+        private const val BASE_URL: String = "http://127.0.0.1:3001"
+
+        private val interceptor: HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
+            this.level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        private val client: OkHttpClient = OkHttpClient.Builder().apply {
+            this.addInterceptor(interceptor)
+        }.build()
+        fun getRetrofitInstance(): Retrofit {
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+    }
 }
