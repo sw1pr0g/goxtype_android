4c40290a28a6c6ea439ef3aff16bc6853462db4b
Alex Poryadin <poryadin_alex@mail.ru>
2023-03-23 11:26:52 +0300

    Full redesigned, recoded StartActivity

--- app/src/main/java/com/sw1pr0g/goxtype_android/StartActivity.kt
+++ app/src/main/java/com/sw1pr0g/goxtype_android/StartActivity.kt
@@ -1,16 +1,19 @@
 package com.sw1pr0g.goxtype_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.get
 import androidx.viewpager2.widget.ViewPager2
 import me.relex.circleindicator.CircleIndicator3
 
 class StartActivity : AppCompatActivity() {
 
+    private lateinit var skipButton: Button
     private lateinit var viewPager2: ViewPager2
-
-
+    private lateinit var nextButton: Button
 
     private var imageList = mutableListOf<Int>()
     private var headingList = mutableListOf<Int>()
@@ -21,17 +24,24 @@ class StartActivity : AppCompatActivity() {
         installSplashScreen()
         setContentView(R.layout.activity_start)
 
+        skipButton = findViewById(R.id.skip_button)
         viewPager2 = findViewById(R.id.start_view_pager2)
+        nextButton = findViewById(R.id.next_button)
+
+        skipButton.setOnClickListener { showAuthActivity() }
 
         viewPager2.adapter = StartViewPagerAdapter2(imageList, headingList, descriptionList)
         viewPager2.orientation = ViewPager2.ORIENTATION_HORIZONTAL
 
         addToList()
 
-
         val indicator = findViewById<CircleIndicator3>(R.id.circle_indicator)
         indicator.setViewPager(viewPager2)
 
+        nextButton.setOnClickListener {
+            if (viewPager2.currentItem == indicator.childCount-1) showAuthActivity()
+            else viewPager2.currentItem += 1
+        }
     }
 
     private fun addToList() {
@@ -54,5 +64,11 @@ class StartActivity : AppCompatActivity() {
         imageList.add(R.drawable.typing_man)
         headingList.add(R.string.start_heading_fourth_slider)
         descriptionList.add(R.string.start_description_fourth_slider)
+    }
+
+    private fun showAuthActivity() {
+        val intent = Intent(this, AuthActivity::class.java)
+        startActivity(intent)
+        this.finish()
     }
 }
